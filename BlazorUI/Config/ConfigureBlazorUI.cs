bd06deda59b33ca382dc148ed70cbd3aa9ca864d
Timothy May <tmay@inscico.com>
2025-09-16 12:10:51 -0700

    interim

--- BlazorUI/Config/ConfigureBlazorUI.cs
+++ BlazorUI/Config/ConfigureBlazorUI.cs
@@ -7,13 +7,4 @@ public static class ConfigureBlazorUI
         services.AddBaseAppBlazorUI();
         return services;
     }
-    public static ILzMessages AddBlazorUIMessages(this ILzMessages lzMessages)
-    {
-        lzMessages.AddBaseAppMessages(); // Add the BaseApp messages    
-        List<string> messages = [
-            "system/{culture}/ConsumerApp/Messages.json",
-            ];
-        lzMessages.MessageFiles.AddRange(messages);
-        return lzMessages;
-    }
 }
